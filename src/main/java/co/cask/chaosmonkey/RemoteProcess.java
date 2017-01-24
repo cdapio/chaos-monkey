3bae6d3e99fecd54244a6ae2e450844737d10856
Denton Liu <liu.denton@gmail.com>
2017-01-24 14:24:59 -0800

    Use a less flaky method to check if service exists

--- src/main/java/co/cask/chaosmonkey/RemoteProcess.java
+++ src/main/java/co/cask/chaosmonkey/RemoteProcess.java
@@ -140,8 +140,6 @@ public class RemoteProcess {
    */
   public boolean exists() throws JSchException {
     LOG.debug("Checking if {} exists", getName());
-    //TODO: There should be a less sketchy method of figuring out if a service exists
-    return sshShell.exec(String.format("sudo service %s 2>&1 | grep -q '%s: unrecognized service'",
-                                       getName(), getName())).returnCode == 1;
+    return sshShell.exec(String.format("test -e /etc/init.d/%s", getName())).returnCode == 0;
   }
 }
